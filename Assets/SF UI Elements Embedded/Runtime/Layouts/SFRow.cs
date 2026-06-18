e9a8d11a7401c1b42d3d42c4dfeeef1c7cf7f123
8BitsPerByte <jonathan.westfall1@gmail.com>
2026-06-18 18:05:31 -0500

    Added the first set of SF Utilities and the first SF Row class.

---
+++ Assets/SF UI Elements Embedded/Runtime/Layouts/SFRow.cs
@@ -0,0 +1,63 @@
+using System.Collections.Generic;
+using UnityEngine.UIElements;
+
+namespace SF.UIModule
+{
+    [UxmlElement]
+    public partial class SFRow : VisualElement
+    {
+        [UxmlAttribute(TwoColumnUSSClassName)] public bool TwoColumn { get; set; }
+        
+        public const string USSClassName = "sf-row";
+        public const string TwoColumnUSSClassName = "two-column";
+
+        public SFRow()
+        {
+            AddToClassList(USSClassName);
+            if(TwoColumn)
+                AddToClassList(TwoColumnUSSClassName);
+        }
+
+        /// <summary>
+        /// Constructor to create a new <see cref="SFRow"/> with an extra default USS class name.
+        /// </summary>
+        /// <param name="className"></param>
+        public SFRow(string className) : this()
+        {
+            if(!string.IsNullOrEmpty(className))
+                AddToClassList(className);
+        }
+
+        /// <summary>
+        /// Constructor to create a new <see cref="SFRow"/> with an array of extra default USS class names.
+        /// </summary>
+        /// <param name="className"></param>
+        public SFRow(string[] classNames) : this()
+        {
+            if(classNames.Length < 1)
+                return;
+
+            for(int i = 0; i < classNames.Length; i++)
+            {
+                if(!string.IsNullOrEmpty(classNames[i]))
+                    AddToClassList(classNames[i]);
+            }
+        }
+        
+        /// <summary>
+        /// Constructor to create a new <see cref="SFRow"/> with a list of extra default USS class names.
+        /// </summary>
+        /// <param name="className"></param>
+        public SFRow(List<string> classNames) : this()
+        {
+            if(classNames.Count < 1)
+                return;
+
+            for(int i = 0; i < classNames.Count; i++)
+            {
+                if(!string.IsNullOrEmpty(classNames[i]))
+                    AddToClassList(classNames[i]);
+            }
+        }
+    }
+}
